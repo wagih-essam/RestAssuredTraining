6e2a4740bed29072795f76a8deccf061a50384fe
MohamedW4 <wagih.mohamed@vodafone.com.eg>
2023-08-16 16:09:37 +0300

    extract and logging

--- src/test/java/com/QACart/C_extract.java
+++ src/test/java/com/QACart/C_extract.java
@@ -1,4 +1,52 @@
 package com.QACart;
 
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+import static org.hamcrest.Matchers.*;
+
 public class C_extract {
+
+    @Test
+    public void firstextract() {
+        Response res = given().baseUri("https://60c391c82df2cb00178ab775.mockapi.io")
+                .when().get("/api/v1/Students")
+                .then().extract().response();
+
+        System.out.println(res.asString());
+    }
+
+    @Test
+    public void extractspecificitem() {
+        Response res = given().baseUri("https://60c391c82df2cb00178ab775.mockapi.io")
+                .when().get("/api/v1/Students")
+                .then().extract().response();
+
+        String firstname = res.path("[0].FirstName");
+        System.out.println(firstname);
+    }
+
+    @Test
+    public void extractitemusingjson() {
+        Response res = given().baseUri("https://60c391c82df2cb00178ab775.mockapi.io")
+                .when().get("/api/v1/Students")
+                .then().extract().response();
+
+        String firstname = JsonPath.from(res.asString()).getString("[0].FirstName");
+
+        System.out.println(firstname);
+    }
+
+    @Test
+    public void extractusingthen() {
+        String firstname = given().baseUri("https://60c391c82df2cb00178ab775.mockapi.io")
+                .when().get("/api/v1/Students")
+                .then().extract().response().path("[0].FirstName");
+
+        System.out.println(firstname);
+    }
 }
--- src/test/java/com/QACart/D_logging.java
+++ src/test/java/com/QACart/D_logging.java
@@ -1,4 +1,82 @@
 package com.QACart;
 
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
 public class D_logging {
+
+    @Test
+    public void logrequestdata_all() {
+        given().baseUri("https://60c391c82df2cb00178ab775.mockapi.io").log().all()
+                    .when().get("/api/v1/Students")
+                    .then();
+        }
+
+    @Test
+    public void logspecificrequestdata_headers() {
+        given().baseUri("https://60c391c82df2cb00178ab775.mockapi.io").log().headers()
+                .when().get("/api/v1/Students")
+                .then();
+    }
+
+    @Test
+    public void logspecificrequestdata_body() {
+        given().baseUri("https://60c391c82df2cb00178ab775.mockapi.io").log().body()
+                .when().get("/api/v1/Students")
+                .then();
+    }
+
+    @Test
+    public void logspecificrequestdata_parameters() {
+        given().baseUri("https://60c391c82df2cb00178ab775.mockapi.io").log().parameters()
+                .when().get("/api/v1/Students")
+                .then();
+    }
+
+    @Test
+    public void logspecificrequestdata_status() {
+        given().baseUri("https://60c391c82df2cb00178ab775.mockapi.io")
+                .when().get("/api/v1/Students")
+                .then().log().status();
+    }
+
+    @Test
+    public void logspecificrequestdata_method() {
+        given().baseUri("https://60c391c82df2cb00178ab775.mockapi.io").log().method()
+                .when().get("/api/v1/Students")
+                .then();
+    }
+
+    @Test
+    public void logspecificrequest_iferrorpositive() {
+        given().baseUri("https://60c391c82df2cb00178ab775.mockapi.io")
+                .when().get("/api/v1/Students")
+                .then().log().ifError();
+    }
+
+    @Test
+    public void logspecificrequest_iferrornegative() {
+        given().baseUri("https://60c391c82df2cb00178ab77.mockapi.io")
+                .when().get("/api/v1/Students")
+                .then().log().ifError();
+    }
+
+    @Test
+    public void logspecificrequest_ifvalidationfailed() {
+        given().baseUri("https://60c391c82df2cb00178ab775.mockapi.io")
+                .when().get("/api/v1/Students")
+                .then().log().ifValidationFails().
+                body("[0].FirstName",equalTo("Giza"));
+    }
+
+    @Test
+    public void logspecificrequest_ifvalidationfailedsecoand() {
+        given().baseUri("https://60c391c82df2cb00178ab775.mockapi.io")
+                .when().get("/api/v1/Students")
+                .then().log().ifValidationFails().
+                body("[0].FirstName",equalTo("Giza"));
+    }
+
 }
